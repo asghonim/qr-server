7a4b195a72cf985940fcc9e32e5e5b453334fd6e
Ahmed Ghonim <asghonim@live.com>
2026-05-20 18:55:18 +0000

    Add devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,5 @@
+npx playwright install --with-deps && npm install -g @playwright/cli;
+playwright-cli install-browser chromium
+npm install
+npx shadcn@latest add --all --yes
+[ -f "$BROWSER" ] && ! command -v xdg-open > /dev/null && sudo ln -s "$BROWSER" /usr/local/bin/xdg-open
