16f01891a8d8ef0a8038e7e5482e1b2dd2c7cd0a
Ahmed Ghonim <asghonim@live.com>
2026-05-28 08:16:33 +0000

    build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import tailwindAnimate from 'tailwindcss-animate'
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [tailwindAnimate],
 }
