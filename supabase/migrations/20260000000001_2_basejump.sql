7b79bfb690ec10c89e5412934f3b7ee2fc5de4d9
Ahmed <asghonim@live.com>
2026-07-03 15:58:47 +0300

    no_pgtab
    This pull request removes test-related SQL setup from the 20260000000001_2_basejump.sql migration file. The following changes were made:
    
    Test setup removal:
    
    Removed creation of the pgtap extension and related query.
    Removed installation and creation of the basejump-supabase_test_helpers extension.

--- supabase/migrations/20260000000001_2_basejump.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgtap;
-SELECT * FROM pg_available_extensions WHERE name = 'pgtap';
-
-select dbdev.install('basejump-supabase_test_helpers');
-CREATE EXTENSION IF NOT EXISTS "basejump-supabase_test_helpers";
